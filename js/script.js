871b87caa6e1f846f833c34601fa1e47144f3e3c
md-saheb-web03 <md1426317@gmail.com>
2024-11-20 13:37:07 +0530

    shop

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// loader code 
+let spinner = document.querySelector('.swiper-wrapper');
+
+window.addEventListener('load', () => {
+  
+    // spinner.style.display = "none";
+
+    setTimeout(() => {
+        spinner.style.display = "none";
+    },500);
+
+});
